ca984d0079fdb3a0c09fc1206aefabe7c7c08b54
@mahny_b <mahny.brand@gmail.com>
2018-06-04 23:30:59 +0900

    Pattern使わないと遅いよと指摘を受けたので検証コードに追加した

---
+++ src/jp/gr/mahny_conf/replaceall_charat/SpeedCrazyPattern.java
@@ -0,0 +1,63 @@
+package jp.gr.mahny_conf.replaceall_charat;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.regex.Pattern;
+
+/**
+ * {@link Pattern#compile(String).replaceAll(String, String)}で制御文字を空文字に置換するよ！
+ * @author mahny
+ *
+ */
+public class SpeedCrazyPattern {
+
+	private static Pattern pattern = Pattern.compile("\\p{Cntrl}");
+
+	/**
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws IOException {
+
+		File f = new File("resources", "test.txt");
+		if (!f.exists()) {
+			System.out.println("テストデータが無い！ / file=[" + f.getAbsolutePath() + "]");
+			return;
+		}
+
+		String testData = null;
+		try (FileInputStream in = new FileInputStream(f);
+				ByteArrayOutputStream byteArrays = new ByteArrayOutputStream()) {
+
+			byte[] buff = new byte[4096];
+			int size = in.read(buff);
+			while (size != -1) {
+				byteArrays.write(buff, 0, size);
+				size = in.read(buff);
+			}
+			byteArrays.flush();
+			testData = new String(byteArrays.toByteArray());
+		}
+
+		int trials = 1000;
+		long startTime = System.currentTimeMillis();
+		for (int i = 0; i < trials; i++) {
+			test(testData);
+		}
+		long endTime = System.currentTimeMillis();
+		long totalTime = endTime - startTime;
+
+		System.out.println("合計時間 : " + totalTime + "(ms), 平均時間 : " + (totalTime / trials) + "(ms)");
+	}
+
+	/**
+	 * 指定された文字列から制御文字を除去した文字列を生成する
+	 * @param org 指定された文字
+	 * @return 制御文字を除去した文字列
+	 */
+	public static String test(String org) {
+		return pattern.matcher(org).replaceAll("");
+	}
+}
